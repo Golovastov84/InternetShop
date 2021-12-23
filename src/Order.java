9f3aa355fb8dc60993273cbbe0ad0796e3c07c1d
Vasya Golovastov <vsgolovastov@gmail.com>
2021-12-23 11:16:43 +0400

    del_//_231221

--- src/Order.java
+++ src/Order.java
@@ -1,6 +1,5 @@
 public class Order {
     public void addProduct(Product product) {
-        // add product
         if(product.getPrice() > 50) {
             product.setPrice(product.getPrice() - 25);
         }
